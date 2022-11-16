6f21a37533a800dd761a7a45558112c27f866926
Seremba <pserembae@gmail.com>
2022-11-16 10:47:40 +0300

    implement search logic

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
@@ -11,8 +12,15 @@ from .forms import RoomForm
 from django.http import HttpResponse
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else  ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    topics = Topic.objects.all()
+    context = {'rooms':rooms, 'topics':topics}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
