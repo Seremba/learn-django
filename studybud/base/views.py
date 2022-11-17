7bc79d1f0cbf517a909848657bc66e94b4403a93
Seremba <pserembae@gmail.com>
2022-11-17 16:30:45 +0300

    redirect user to home page if he is loggedc in already

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -17,6 +17,9 @@ from .forms import RoomForm
 from django.http import HttpResponse
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
